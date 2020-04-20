aeb0d5ee7442ba8563d86ae636ce543954851695
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-04-20 14:21:20 -0700

    added session support

--- api/server.js
+++ api/server.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const helmet = require("helmet");
+const session = require("express-session");
 
 const userRouter = require("../users/users-router");
 const authRouter = require("../auth/authRouter");
 
 const server = express();
 
+const sessionConfig = {
+  name: "cookie",
+  secret: process.env.SESSION_SECRET || "secret",
+  resave: false,
+  saveUninitialized: process.env.SEND_COOKIES || true,
+  cookie: {
+    maxAge: 1000 * 60 * 1, // 1 minute
+    secure: process.env.SECURE_COOKIES || false,
+    httpOnly: true,
+  },
+};
+
 server.use(express.json());
 server.use(helmet());
+server.use(session(sessionConfig));
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "api running" });
